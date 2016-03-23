9c93eef3ed04ac985017f99b634ff95e9988bd38
Cai <CaiB@AD.SEC.GOV>
2016-03-23 10:35:32 -0400

    delete files

--- static/gulp/watch.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-
-gulp.task('watch', ['build'], function () {
-  gulp.watch('src/**/*.js', ['scripts']);
-  gulp.watch('src/**/*.styl', ['styles']);
-});
--- static/src/modules/common/components/MenuBar/KeyboardNavItems.js
+++
@@ -1,4 +0,0 @@
-export default {
-    // 70: 'dashboard', //"F"
-    // 123: function() { window.alert('test')}, //<-- This works, too.
-};
